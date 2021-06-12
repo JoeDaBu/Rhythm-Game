a0d9d732507efb7d53d6d48f6fae7254bf7e9183
Angelo Gonzales <awesomenesspiza@gmail.com>
2021-06-11 20:54:07 -0600

    Test

--- RhythmGame/desktop/src/gamejam/rhythm/game/desktop/DesktopLauncher.java
+++ RhythmGame/desktop/src/gamejam/rhythm/game/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import gamejam.rhythm.game.Rhythm;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "AngeloTheBoss";
+		config.title = "JoeIsNotTheBoss";
 		config.width = 800;
 		config.height = 800;
 		new LwjglApplication(new Rhythm(), config);
