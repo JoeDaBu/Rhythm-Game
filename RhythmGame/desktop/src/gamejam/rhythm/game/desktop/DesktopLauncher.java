3fe7eb84c978e684f12e49f0a0d9bfa2db6785e6
mohamedyassin1 <73013959+mohamedyassin1@users.noreply.github.com>
2021-06-11 21:06:03 -0600

    Angelo Can't Spell.
    Rhythm.

--- RhythmGame/desktop/src/gamejam/rhythm/game/desktop/DesktopLauncher.java
+++ RhythmGame/desktop/src/gamejam/rhythm/game/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import gamejam.rhythm.game.Rhythm;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Rythm Game";
+		config.title = "Rhythm Game";
 		config.width = 1280;
 		config.height = 720;
 		new LwjglApplication(new Rhythm(), config);
