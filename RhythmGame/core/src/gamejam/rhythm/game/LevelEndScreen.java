27a3743ea9b4ae7b5d486530a355c65ee2636b65
joe <joegbu@gmail.com>
2021-06-13 00:40:36 -0600

    score assets

--- RhythmGame/core/src/gamejam/rhythm/game/LevelEndScreen.java
+++ RhythmGame/core/src/gamejam/rhythm/game/LevelEndScreen.java
@@ -1,7 +1,6 @@
 package gamejam.rhythm.game;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -9,9 +8,9 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class LevelEndScreen implements Screen{
+public class LevelEndScreen extends MainMenuScreen{
 
-	private final Rhythm game;
+
 	private OrthographicCamera camera;
 
 	private String levelName = "";
@@ -37,17 +36,9 @@ public class LevelEndScreen implements Screen{
 	Texture playButtonInactive;
 
 	public LevelEndScreen(final Rhythm game, String levelName, int highScore) {
-		this.game = game;
+        super(game);
 		this.levelName = levelName;
 		this.highScore = highScore;
-		
-		playButtonActive = new Texture(Gdx.files.internal("Buttons/play.png"));
-        playButtonInactive = new Texture(Gdx.files.internal("Buttons/play_in.png"));
-        exitButtonActive = new Texture(Gdx.files.internal("Buttons/exit.png"));
-        exitButtonInactive = new Texture(Gdx.files.internal("Buttons/exit_in.png"));
-
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, Rhythm.WIDTH, Rhythm.HEIGHT);
 	}
 
 	@Override
